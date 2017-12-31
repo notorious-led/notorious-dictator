ab7f43dc904f0f141a3348d4c7e965f5c2135b9d
Hunter Fuller <hfuller@pixilic.com>
2017-12-30 23:02:38 -0600

    ignore this the code is terrible
    this needs a total rewrite

--- cli.py
+++ cli.py
@@ -4,6 +4,9 @@ import sys
 import datetime
 import requests
 import numpy
+import socket
+import select
+from time import sleep
 
 ping_count = 100
 
@@ -14,6 +17,27 @@ def ping(endpoint):
         requests.get(url) #and throw it in the garbage
         time = datetime.datetime.now() - time
         return(time.total_seconds() * 1000)
+    elif endpoint['type'] == 'glowup-over-tcp':
+        sleep(0.05)
+
+        host = endpoint["location"].split(':')[0]
+        port = int(endpoint["location"].split(':')[1])
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        sock.setblocking(0)
+
+        time = datetime.datetime.now()
+        ready = select.select([sock], [], [], 5)
+        sock.send(bytes('m\r\n', 'ascii'))
+        if ready[0]:
+            data = sock.recv(4096)
+        sock.close()
+
+        if bytes('*************', 'ascii') in data:
+            time = datetime.datetime.now() - time
+            return(time.total_seconds() * 1000)
+        else:
+            return 250
     else:
         raise NotImplementedError
 
@@ -33,6 +57,13 @@ def send_rtt(endpoint):
     if endpoint['type'] == 'hoopla':
         url = "http://" + endpoint["location"] + "/sync/rtt"
         requests.post(url, data = { 'rtt': str(endpoint["rtt"]) })
+    elif endpoint['type'] == 'glowup-over-tcp':
+        host = endpoint["location"].split(':')[0]
+        port = int(endpoint["location"].split(':')[1])
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        sock.send(bytes('rtt ' + str(endpoint['rtt']) + '\r\n', 'ascii'))
+        sock.close()
     else:
         raise NotImplementedError
 
@@ -44,7 +75,7 @@ if __name__ == '__main__':
     print("Loaded endpoints:", [e["name"] for e in endpoints])
     
     while True:
-        sys.stdout.write("\nCommands: Calibrate, Ping, Schedule, 0-9 (immediate) > ")
+        sys.stdout.write("\nCommands: Calibrate, Ping, Send, 0-9 (immediate) > ")
         sys.stdout.flush()
     
         ch = readchar.readkey()[0]
@@ -62,3 +93,18 @@ if __name__ == '__main__':
                 sys.stdout.flush()
                 send_rtt(endpoint)
                 sys.stdout.write("done.\n")
+        elif ch.lower() == 'p':
+            for endpoint in endpoints:
+                sys.stdout.write("Ping " + endpoint["name"] + ": ")
+                sys.stdout.flush()
+                print(ping(endpoint))
+        elif ch.lower() == 's':
+            thing = input("Send what? ")
+            for endpoint in endpoints:
+                host = endpoint["location"].split(':')[0]
+                port = int(endpoint["location"].split(':')[1])
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect((host, port))
+                sock.send(bytes(thing + '\r\n', 'ascii'))
+                sock.close()
+                
